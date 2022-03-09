58dd2e3153c1031f62b9b714911afd00b7de2a51
manoharej <76063339+manoharej@users.noreply.github.com>
2022-03-09 18:29:20 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-# Creating Security Group for ELB
+# Creating EC2 instance public and private network 
 resource "aws_instance" "demoec2" {
     ami= "ami-0e1d30f2c40c4c701"
     instance_type = "t2.micro"
